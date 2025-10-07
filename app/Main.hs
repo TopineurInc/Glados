42eb18e10da3782b74cd013b29a97aa6c1d62a07
William Jolivet <william.jolivet@epitech.eu>
2025-10-07 13:18:37 +0200

    feat: add compiled code display functionality to Main.hs
    - Introduced a new command-line option `--compiled` to show the compiled code of a LISP file.
    - Implemented the `showCompiled` function to handle the compilation and display of code and nested definitions.
    - Updated the help message to include usage instructions for the new `--compiled` option.

--- app/Main.hs
+++ app/Main.hs
@@ -18,6 +18,7 @@ import VM
 import Disasm
 import SExprParser
 import Desugar
+import qualified Data.Vector as Vector
 
 main :: IO ()
 main = do
@@ -27,6 +28,7 @@ main = do
     ["--help"] -> printHelp
     ["--disasm", file] -> disasmFile file
     ["--ast", file] -> showAst file
+    ["--compiled", file] -> showCompiled file
     [file] -> runFile file
     _ -> do
       hPutStrLn stderr "Invalid arguments. Use --help for usage."
@@ -37,16 +39,18 @@ printHelp = do
   putStrLn "GLaDOS - A LISP compiler and VM"
   putStrLn ""
   putStrLn "Usage:"
-  putStrLn "  glados-exe [file]           Compile and run a LISP file"
-  putStrLn "  glados-exe --disasm [file]  Disassemble a LISP file"
-  putStrLn "  glados-exe --ast [file]     Show AST of a LISP file"
-  putStrLn "  glados-exe                  Start REPL (interactive mode)"
-  putStrLn "  glados-exe --help           Show this help message"
+  putStrLn "  glados-exe [file]             Compile and run a LISP file"
+  putStrLn "  glados-exe --disasm [file]    Disassemble a LISP file"
+  putStrLn "  glados-exe --ast [file]       Show AST of a LISP file"
+  putStrLn "  glados-exe --compiled [file]  Show compiled code of a LISP file"
+  putStrLn "  glados-exe                    Start REPL (interactive mode)"
+  putStrLn "  glados-exe --help             Show this help message"
   putStrLn ""
   putStrLn "Examples:"
   putStrLn "  glados-exe program.lisp"
   putStrLn "  glados-exe --disasm program.lisp"
   putStrLn "  glados-exe --ast program.lisp"
+  putStrLn "  glados-exe --compiled program.lisp"
 
 -- Run a file
 runFile :: FilePath -> IO ()
@@ -107,6 +111,33 @@ showAst file = do
         Right exprs -> do
           mapM_ (putStrLn . show) exprs
           exitSuccess
+
+-- Show compiled code of a file
+showCompiled :: FilePath -> IO ()
+showCompiled file = do
+  source <- readFile file
+  case compileWithDefs defaultConfig source of
+    Left err -> do
+      hPutStrLn stderr $ "Compilation error: " ++ show err
+      exitFailure
+    Right (code, defs) -> do
+      putStrLn "=== Main Code ==="
+      dumpCodeInfo code
+      putStrLn "\n=== Nested Definitions ==="
+      mapM_ (\(name, obj) -> do
+        putStrLn $ "\n--- " ++ name ++ " ---"
+        dumpCodeInfo obj) (Map.toList defs)
+      exitSuccess
+
+dumpCodeInfo :: CodeObject -> IO ()
+dumpCodeInfo co = do
+  putStrLn $ "Name: " ++ coName co
+  putStrLn $ "Arity: " ++ show (coArity co)
+  putStrLn $ "Max Locals: " ++ show (coMaxLocals co)
+  putStrLn "\nConstants:"
+  Vector.imapM_ (\i c -> putStrLn $ "  " ++ show i ++ ": " ++ show c) (coConsts co)
+  putStrLn "\nInstructions:"
+  Vector.imapM_ (\i instr -> putStrLn $ "  " ++ show i ++ ": " ++ show instr) (coInstrs co)
 
 -- Simple REPL
 repl :: IO ()
