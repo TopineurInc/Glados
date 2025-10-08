5fe47c0466d8aae0ab2956198ed9cecd644df51a
William Jolivet <william.jolivet@epitech.eu>
2025-10-08 14:22:58 +0200

    refactor(examples): rename Fibonacci iteration function for consistency

--- examples/fibonacci_10.lisp
+++ examples/fibonacci_10.lisp
@@ -1,9 +1,9 @@
-(define (fibiter a b count)
+(define (fib_iter a b count)
   (if (eq? count 0)
       a
-      (fibiter b (+ a b) (- count 1))))
+      (fib_iter b (+ a b) (- count 1))))
 
 (define (fib n)
-  (fibiter 0 1 n))
+  (fib_iter 0 1 n))
 
 (fib 10)
