77b5e5af4106ec189d114d41e3d62ee2c14a0b40
William Jolivet <william.jolivet@epitech.eu>
2025-10-08 14:51:01 +0200

    refactor(main): streamline error handling and improve help text formatting

--- app/Main.hs
+++ app/Main.hs
@@ -33,13 +33,13 @@ main = do
     ["--compiled", file] -> showCompiled file
     ["--bytecode", file] -> showBytecode file
     [file] -> runFile file
-    _ -> do
+    _ ->
       exitWithError "Invalid arguments. Use --help for usage."
 
 exitWithError :: String -> IO a
-exitWithError msg = do
-  hPutStrLn stderr $ "*** ERROR : " ++ msg
-  exitWith (ExitFailure 84)
+exitWithError msg =
+  hPutStrLn stderr ("*** ERROR : " ++ msg)
+    >> exitWith (ExitFailure 84)
 
 formatCompileError :: CompileError -> String
 formatCompileError (ParseError err _) = "ParseError " ++ err
@@ -54,23 +54,26 @@ renderValue (VClosure _ _) = "#<procedure>"
 renderValue (VBuiltin name _) = "#<builtin:" ++ name ++ ">"
 
 printHelp :: IO ()
-printHelp = do
-  putStrLn "GLaDOS - A LISP compiler and VM"
-  putStrLn ""
-  putStrLn "Usage:"
-  putStrLn "  ./glados [file]             Compile and run a LISP file"
-  putStrLn "  ./glados --disasm [file]    Disassemble a LISP file"
-  putStrLn "  ./glados --ast [file]       Show AST of a LISP file"
-  putStrLn "  ./glados --compiled [file]  Show compiled code of a LISP file"
-  putStrLn "  ./glados --bytecode [file]  Show bytecode instructions of a LISP file"
-  putStrLn "  ./glados                    Start REPL (interactive mode)"
-  putStrLn "  ./glados --help             Show this help message"
-  putStrLn ""
-  putStrLn "Examples:"
-  putStrLn "  ./glados program.lisp"
-  putStrLn "  ./glados --disasm program.lisp"
-  putStrLn "  ./glados --ast program.lisp"
-  putStrLn "  ./glados --compiled program.lisp"
+printHelp =
+  mapM_
+    putStrLn
+    [ "GLaDOS - A LISP compiler and VM"
+    , ""
+    , "Usage:"
+    , "  ./glados [file]             Compile and run a LISP file"
+    , "  ./glados --disasm [file]    Disassemble a LISP file"
+    , "  ./glados --ast [file]       Show AST of a LISP file"
+    , "  ./glados --compiled [file]  Show compiled code of a LISP file"
+    , "  ./glados --bytecode [file]  Show bytecode instructions of a LISP file"
+    , "  ./glados                    Start REPL (interactive mode)"
+    , "  ./glados --help             Show this help message"
+    , ""
+    , "Examples:"
+    , "  ./glados program.lisp"
+    , "  ./glados --disasm program.lisp"
+    , "  ./glados --ast program.lisp"
+    , "  ./glados --compiled program.lisp"
+    ]
 
 -- Run a file
 runFile :: FilePath -> IO ()
@@ -88,9 +91,9 @@ runFile file = do
       case execResult of
         Left ex -> exitWithError $ "Runtime error: " ++ show ex
         Right (Left err) -> exitWithError $ "Runtime error: " ++ show err
-        Right (Right val) -> do
-          putStrLn $ renderValue val
-          exitSuccess
+        Right (Right val) ->
+          putStrLn (renderValue val)
+            >> exitSuccess
 
 -- Disassemble a file
 disasmFile :: FilePath -> IO ()
@@ -101,14 +104,16 @@ disasmFile file = do
     Right src -> return src
   case compileWithDefs defaultConfig source of
     Left err -> exitWithError (formatCompileError err)
-    Right (code, defs) -> do
+    Right (code, defs) ->
       putStrLn "=== Main Code ==="
-      dumpCodeObject code
-      putStrLn "\n=== Nested Definitions ==="
-      mapM_ (\(name, obj) -> do
-        putStrLn $ "\n--- " ++ name ++ " ---"
-        dumpCodeObject obj) (Map.toList defs)
-      exitSuccess
+        >> dumpCodeObject code
+        >> putStrLn "\n=== Nested Definitions ==="
+        >> mapM_
+          (\(name, obj) ->
+            putStrLn ("\n--- " ++ name ++ " ---")
+              >> dumpCodeObject obj)
+          (Map.toList defs)
+        >> exitSuccess
 
 -- Show AST of a file
 showAst :: FilePath -> IO ()
@@ -119,15 +124,13 @@ showAst file = do
     Right src -> return src
   case parseFromString source of
     Left err -> exitWithError (formatCompileError err)
-    Right sexprs -> do
+    Right sexprs ->
       putStrLn "=== S-Expression ==="
-      mapM_ (putStrLn . show) sexprs
-      putStrLn "\n=== AST ==="
-      case mapM sexprToExpr sexprs of
-        Left err -> exitWithError ("Desugar error: " ++ show err)
-        Right exprs -> do
-          mapM_ (putStrLn . show) exprs
-          exitSuccess
+        >> mapM_ (putStrLn . show) sexprs
+        >> putStrLn "\n=== AST ==="
+        >> case mapM sexprToExpr sexprs of
+          Left err -> exitWithError ("Desugar error: " ++ show err)
+          Right exprs -> mapM_ (putStrLn . show) exprs >> exitSuccess
 
 -- Show compiled code of a file
 showCompiled :: FilePath -> IO ()
@@ -138,14 +141,16 @@ showCompiled file = do
     Right src -> return src
   case compileWithDefs defaultConfig source of
     Left err -> exitWithError (formatCompileError err)
-    Right (code, defs) -> do
+    Right (code, defs) ->
       putStrLn "=== Main Code ==="
-      dumpCodeInfo code
-      putStrLn "\n=== Nested Definitions ==="
-      mapM_ (\(name, obj) -> do
-        putStrLn $ "\n--- " ++ name ++ " ---"
-        dumpCodeInfo obj) (Map.toList defs)
-      exitSuccess
+        >> dumpCodeInfo code
+        >> putStrLn "\n=== Nested Definitions ==="
+        >> mapM_
+          (\(name, obj) ->
+            putStrLn ("\n--- " ++ name ++ " ---")
+              >> dumpCodeInfo obj)
+          (Map.toList defs)
+        >> exitSuccess
 
 -- Show raw bytecode instructions of a file
 showBytecode :: FilePath -> IO ()
@@ -156,29 +161,37 @@ showBytecode file = do
     Right src -> return src
   case compileWithDefs defaultConfig source of
     Left err -> exitWithError (formatCompileError err)
-    Right (code, defs) -> do
+    Right (code, defs) ->
       putStrLn "=== Main Bytecode ==="
-      dumpBytecode code
-      putStrLn "\n=== Nested Definitions ==="
-      mapM_ (\(name, obj) -> do
-        putStrLn $ "\n--- " ++ name ++ " ---"
-        dumpBytecode obj) (Map.toList defs)
-      exitSuccess
+        >> dumpBytecode code
+        >> putStrLn "\n=== Nested Definitions ==="
+        >> mapM_
+          (\(name, obj) ->
+            putStrLn ("\n--- " ++ name ++ " ---")
+              >> dumpBytecode obj)
+          (Map.toList defs)
+        >> exitSuccess
 
 dumpCodeInfo :: CodeObject -> IO ()
-dumpCodeInfo co = do
-  putStrLn $ "Name: " ++ coName co
-  putStrLn $ "Arity: " ++ show (coArity co)
-  putStrLn $ "Max Locals: " ++ show (coMaxLocals co)
-  putStrLn "\nConstants:"
-  Vector.imapM_ (\i c -> putStrLn $ "  " ++ show i ++ ": " ++ show c) (coConsts co)
-  putStrLn "\nInstructions:"
-  Vector.imapM_ (\i instr -> putStrLn $ "  " ++ show i ++ ": " ++ show instr) (coInstrs co)
+dumpCodeInfo co =
+  putStrLn ("Name: " ++ coName co)
+    >> putStrLn ("Arity: " ++ show (coArity co))
+    >> putStrLn ("Max Locals: " ++ show (coMaxLocals co))
+    >> putStrLn "\nConstants:"
+    >> Vector.imapM_
+      (\i c -> putStrLn $ "  " ++ show i ++ ": " ++ show c)
+      (coConsts co)
+    >> putStrLn "\nInstructions:"
+    >> Vector.imapM_
+      (\i instr -> putStrLn $ "  " ++ show i ++ ": " ++ show instr)
+      (coInstrs co)
 
 dumpBytecode :: CodeObject -> IO ()
-dumpBytecode co = do
+dumpBytecode co =
   putStrLn "Instructions:"
-  Vector.imapM_ (\i instr -> putStrLn $ "  " ++ show i ++ ": " ++ show instr) (coInstrs co)
+    >> Vector.imapM_
+      (\i instr -> putStrLn $ "  " ++ show i ++ ": " ++ show instr)
+      (coInstrs co)
 
 -- Simple REPL
 repl :: IO ()
@@ -190,18 +203,17 @@ repl = do
 
 replLoop :: Bool -> IO ()
 replLoop interactive = do
-  when interactive $ do
-    putStr "> "
-    hFlush stdout
+  when interactive $
+    putStr "> " >> hFlush stdout
   eof <- isEOF
   if eof
     then when interactive (putStrLn "")
     else do
       input <- getLine
       case compile defaultConfig input of
-        Left err -> do
-          putStrLn $ "*** ERROR : " ++ formatCompileError err
-          replLoop interactive
+        Left err ->
+          putStrLn ("*** ERROR : " ++ formatCompileError err)
+            >> replLoop interactive
         Right code -> do
           let vmState = initVMState { vCodeObjects = Map.singleton "main" code }
           execResult <- try (execVM vmState code) :: IO (Either SomeException (Either VMError Value))
